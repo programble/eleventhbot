d474ddc4d74abc862ba750a8353cf6a6c6709d04
Curtis McEnroe <programble@gmail.com>
2013-09-19 10:48:38 -0400

    Add inform, first, plays Lastfm commands

--- lib/eleventhbot/lastfm.rb
+++ lib/eleventhbot/lastfm.rb
@@ -24,9 +24,9 @@ module EleventhBot
       @chart_top = @lastfm.chart.get_top_artists(:limit => 0).map {|x| x['name'] }
     end
 
-    def api_transaction(m, &block)
+    def api_transaction(m)
       begin
-        block.call
+        yield
       rescue ::Lastfm::ApiError => e
         m.reply("Last.fm error: #{e.message.strip}")
         raise
@@ -75,6 +75,43 @@ module EleventhBot
       api_transaction(m) do
         track = @lastfm.user.get_recent_tracks(user)[index - 1]
         m.reply("#{user}: #{format_track(track)}")
+      end
+    end
+
+    match /inform (#\S+)$/, method: :inform
+    def inform(m, channel)
+      user = pstore_get(m)
+      return m.reply("Your nick is not associated with a Last.fm account", true) unless user
+      api_transaction(m) do
+        track = @lastfm.user.get_recent_tracks(user).first
+        if track['nowplaying']
+          Channel(channel).msg("#{m.user.nick} is listening to #{format_track(track, false)}")
+        else
+          Channel(channel).msg("#{m.user.nick} last listened to #{format_track(track)}")
+        end
+      end
+    end
+
+    match /first$/, method: :first
+    match /first (\S+)$/, method: :first
+    def first(m, user = nil)
+      user ||= pstore_get(m)
+      api_transaction(m) do
+        track = @lastfm.user.get_recent_tracks(:user => user,
+                                               :limit => 1,
+                                               :page => 999999999)
+        m.reply("#{user}: #{format_track(track)}")
+      end
+    end
+
+    match /plays$/, method: :plays
+    match /plays (\S+)$/, method: :plays
+    def plays(m, user = nil)
+      user ||= pstore_get(m)
+      api_transaction(m) do
+        info = @lastfm.user.get_info(user)
+        registered = Time.at(info['registered']['unixtime'].to_i)
+        m.reply("#{user}: #{info['playcount']} plays since #{registered.strftime('%d %b %Y')}")
       end
     end
   end
