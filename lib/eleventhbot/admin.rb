9614d59398b929f60de2cca26bdcfa4ab3ddb9c1
Curtis McEnroe <programble@gmail.com>
2013-09-29 13:16:07 -0400

    Move admin check into a hook

--- lib/eleventhbot/admin.rb
+++ lib/eleventhbot/admin.rb
@@ -6,6 +6,7 @@ module EleventhBot
       option_array :masks, String
     end
 
+    hook :pre, method: :admin?
     def admin?(m)
       admin = config.masks.any? {|mask| m.user.match(mask) }
       m.reply('You are not an administrator', true) unless admin
@@ -15,31 +16,31 @@ module EleventhBot
     command :say, /say (\S+) (.+)/,
       'say {target} {thing}: Say something'
     def say(m, target, s)
-      Target(target).msg(s) if admin?(m)
+      Target(target).msg(s)
     end
 
     command :action, /action (\S+) (.+)/,
       'action {target} {thing}: Do something'
     def action(m, target, s)
-      Target(target).action(s) if admin?(m)
+      Target(target).action(s)
     end
 
     command :nick, /nick(?: (\S+))?/,
       "nick [nick]: Change the bot's nick"
     def nick(m, nick)
-      @bot.nick = nick || Configru.irc.nick if admin?(m)
+      @bot.nick = nick || Configru.irc.nick
     end
 
     command :join, /join (\S+)(?: (\S+))?/,
       'join {channel} [key]: Join a channel'
     def join(m, channel, key)
-      @bot.join(channel, key) if admin?(m)
+      @bot.join(channel, key)
     end
 
     command :part, /part (\S+)(?: (.+))?/,
       'part {channel} [reason]: Leave a channel'
     def part(m, channel, reason)
-      @bot.part(channel, reason) if admin?(m)
+      @bot.part(channel, reason)
     end
 
     def available_plugins
@@ -61,7 +62,6 @@ module EleventhBot
     command :plugins, /plugins/,
       'plugins: List loaded plugins'
     def plugins(m)
-      return unless admin?(m)
       plugins = available_plugins.map do |plugin|
         if enabled_plugin(plugin.plugin_name)
           plugin.plugin_name
@@ -75,7 +75,6 @@ module EleventhBot
     command :disable, /disable (\S+)/,
       'disable {plugin}: Disable a plugin'
     def disable(m, name)
-      return unless admin?(m)
       if plugin = enabled_plugin(name)
         @bot.plugins.unregister_plugin(plugin)
         m.reply("#{name} disabled", true)
@@ -87,7 +86,6 @@ module EleventhBot
     command :enable, /enable (\S+)/,
       'enable {plugin}: Enable a plugin'
     def enable(m, name)
-      return unless admin?(m)
       return m.reply("#{name} is already enabled", true) if enabled_plugin(name)
       if plugin = available_plugin(name)
         @bot.plugins.register_plugin(plugin)
@@ -100,8 +98,6 @@ module EleventhBot
     command :reload, /reload (\S+)/,
       'reload {plugin}: Reload a plugin'
     def reload(m, name)
-      return unless admin?(m)
-
       if plugin = enabled_plugin(name)
         @bot.plugins.unregister_plugin(plugin)
       else
