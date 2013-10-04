f81e0cc0af139789cdd9a3e690aef03eb3ced43b
Curtis McEnroe <programble@gmail.com>
2013-10-04 18:52:12 -0400

    Add admin commands to Ignore

--- lib/eleventhbot/ignore.rb
+++ lib/eleventhbot/ignore.rb
@@ -11,7 +11,7 @@ module EleventhBot
 
       ignore_hook = proc do |m|
         if m.user
-          !Configru.ignore.masks.any? {|mask| m.user.match(mask) }
+          !plugin(Ignore).config.masks.any? {|mask| m.user.match(mask) }
         else
           true
         end
@@ -31,6 +31,31 @@ module EleventhBot
           __hooks(:pre).delete(@ignore_hook)
         end
       end
+    end
+
+    command :ignores, /ignores/,
+      'ignores: Lists ignored masks',
+      group: :admin
+    def ignores(m)
+      m.reply(config.masks.join(', '), true)
+    end
+
+    command :ignore, /ignore (\S+)/,
+      'ignore {mask}: Ignore commands from users matching a mask',
+      group: :admin
+    def ignore(m, mask)
+      return m.reply('Mask already ignored', true) if config.masks.include? mask
+      config.masks << mask
+      m.reply('Mask ignored', true)
+    end
+
+    command :unignore, /unignore (\S+)/,
+      'unignore {mask}: Stop ignoring commands from users matching a mask',
+      group: :admin
+    def unignore(m, mask)
+      return m.reply('Mask not ignored', true) unless config.masks.include? mask
+      config.masks.delete(mask)
+      m.reply('Mask unignored', true)
     end
   end
 end
