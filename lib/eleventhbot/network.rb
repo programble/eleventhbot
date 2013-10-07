16bc86f142b6afabf8e7b5bfed4c50c974c041db
Curtis McEnroe <programble@gmail.com>
2013-10-06 21:45:09 -0400

    Use Resolv for Network plugin

--- lib/eleventhbot/network.rb
+++ lib/eleventhbot/network.rb
@@ -1,15 +1,22 @@
-require 'socket'
+require 'ipaddr'
+require 'resolv'
 
 module EleventhBot
   class Network
     include Plugin, Cinch::Plugin
 
-    Socket.do_not_reverse_lookup = true
+    def ip? ip
+      !!IPAddr.new(ip) rescue false
+    end
 
-    command :host, /host (.+)/,
-      'host [hostname or IP]: Get IP addresses or reverse DNS for the given query'
+    command :host, /host (\S+)/,
+      'host {hostname or IP}: Get IP addresses or reverse DNS for the given query'
     def host(m, name)
-      m.reply Socket.getaddrinfo(name, nil).map { |f| f[2] }.uniq.join(', ')
+      if ip? name
+        m.reply(Resolv.getnames(name).join(', '), true)
+      else
+        m.reply(Resolv.getaddresses(name).join(', '), true)
+      end
     end
   end
 end
