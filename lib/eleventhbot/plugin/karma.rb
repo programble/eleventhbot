d8431ccdf6aff12e6fc310a4859116a8961782ef
Curtis McEnroe <programble@gmail.com>
2013-10-18 23:56:38 -0400

    Port Karma plugin to Redis

--- lib/eleventhbot/plugin/karma.rb
+++ lib/eleventhbot/plugin/karma.rb
@@ -1,25 +1,11 @@
-require 'pstore'
-
 module EleventhBot
   class Plugin::Karma
     include Plugin, Cinch::Plugin
 
-    configru do
-      option :pstore, String, 'karma.pstore'
-    end
-
-    def initialize(*args)
-      super
-      @pstore = PStore.new(config.pstore)
-    end
-
     match /^(\S+)(\+\+|--)/, use_prefix: false, method: :incdec
     def incdec(m, target, op)
       target.downcase!
-      @pstore.transaction do
-        @pstore[target] ||= 0
-        @pstore[target] += (op == '++') ? 1 : -1
-      end
+      redis.hincrby(:karma, target, (op == '++') ? 1 : -1)
     end
 
     command :karma, /karma(?: (\S+))?/,
@@ -27,9 +13,7 @@ module EleventhBot
     def karma(m, target)
       target ||= m.user.nick
       target.downcase!
-      @pstore.transaction(true) do
-        m.reply("#{target} has #{@pstore[target] || 0} karma")
-      end
+      m.reply("#{target} has #{redis.hget(:karma, target) || 0} karma")
     end
   end
 end
