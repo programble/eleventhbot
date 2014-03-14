eb45b619d6f1246101a49eeac4c33e6c5a39cabd
Scott Olson <scott@scott-olson.org>
2014-03-13 23:53:21 -0600

    Add note about detected language when using auto translation.

--- lib/eleventhbot/plugin/translate.rb
+++ lib/eleventhbot/plugin/translate.rb
@@ -30,7 +30,10 @@ module EleventhBot
       return m.reply('Invalid target language') unless @supported_langs.include?(to)
 
       if from == 'auto'
-        m.reply(@bing.translate(text, to: to))
+        trans = @bing.translate(text, to: to)
+        code = @bing.detect(text).to_s
+        lang = @language_names[code]
+        m.reply("#{trans} [detected as #{lang} (#{code})]")
       else
         return m.reply('Invalid source language') unless @supported_langs.include?(from)
         m.reply(@bing.translate(text, from: from, to: to))
